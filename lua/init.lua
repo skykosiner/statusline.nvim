8c101dec6d6fb3b57048d5f052d384e1e5f473cf
Yoni Kosiner <yoni@kosiner.co.uk>
2022-12-23 20:22:56 +0000

    feat: settings

--- lua/init.lua
+++ lua/init.lua
@@ -1,11 +1,17 @@
 local statusInfo = require("statusInfo.statusInfo")
-local statusLine = {}
+local methods    = require("methods")
+
+local statusLine = {
+    setup = methods.Setup
+}
+
+print(methods.Get())
 
 local statusline = "%%#Modes#" ..
     " %s%%)" .. "%%#Ignore#" .. "%s%% %s%%)  %%-5.100(%s%%) %s%%  %%-5.20(%s%%)%%-6.6)"
 
 function statusLine:status()
-    return string.format(statusline,
+    return string.format(statusline),
         statusInfo.getMode(statusInfo),
         statusInfo.getGitInfo(statusInfo),
         "%=",
