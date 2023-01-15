142c10cf4bffd600d1f33d8f53234b1ca3213bc6
Yoni Kosiner <yoni@kosiner.co.uk>
2023-01-15 23:57:46 +0000

    wip: playing around with colors and toggble options

--- lua/statusLine/init.lua
+++ lua/statusLine/init.lua
@@ -2,36 +2,37 @@ local methods     = require("statusLine.methods")
 local statusInfo  = require("statusLine.statusInfo")
 local lspInfo     = require("statusLine.lspInfo")
 local harpoonInfo = require("statusLine.harpoonInfo")
+local utils       = require("statusLine.utils")
 
 local statusLine = {
-    setup = methods.Setup
+  setup = methods.Setup
 }
 
 -- print(vim.inspect(methods.Get("ShowItems")))
 
 function status()
-    local statusline = "%%#Modes#" .. " %s" .. "%%#Ignore# " .. "%s %s %s %s %s %s %s "
-    return string.format(statusline,
-        statusInfo.getMode(),
-        lspInfo.infoCount(),
-        "%=",
-        statusInfo.getFileInfo(),
-        harpoonInfo.getCurrentMark(),
-        "%=",
-        statusInfo.getGitInfo(),
-        statusInfo.getLineInfo()
-    )
+  local statusline = "%%#Modes#" .. " %s" .. "%%#Ignore# " .. "%s %s %s %s %s %s %s "
+  return string.format(statusline,
+    statusInfo.getMode(),
+    lspInfo.infoCount(),
+    "%=",
+    statusInfo.getFileInfo(),
+    harpoonInfo.getCurrentMark(),
+    "%=",
+    statusInfo.getGitInfo(),
+    statusInfo.getLineInfo()
+  )
 end
 
 function statusLine:setStatus()
-    vim.o.statusline = '%!v:lua.status()'
+  vim.o.statusline = '%!v:lua.status()'
 
-    local colors = {
-        ["backgroundColor"] = methods.Get("colors")["backgroundColor"],
-        ["textColor"] = methods.Get("colors")["textColor"]
-    }
+  local colors = {
+    ["backgroundColor"] = methods.Get("colors")["backgroundColor"],
+    ["textColor"] = methods.Get("colors")["textColor"]
+  }
 
-    vim.api.nvim_set_hl(0, "Ignore", { bg = colors["backgroundColor"], fg = colors["textColor"] })
+  vim.api.nvim_set_hl(0, "Ignore", { bg = colors["backgroundColor"], fg = colors["textColor"] })
 end
 
 return statusLine
--- lua/statusLine/statusInfo.lua
+++ lua/statusLine/statusInfo.lua
@@ -1,79 +1,80 @@
 local statusInfo = {}
 
 function statusInfo.getFileInfo()
-    local name = vim.api.nvim_buf_get_name(0)
+  local name = vim.api.nvim_buf_get_name(0)
 
-    if not name or name == "" then
-        return "[No Name]"
-    end
+  if not name or name == "" then
+    return "[No Name]"
+  end
 
-    local file_name, file_ext = vim.fn.expand("%:t"), vim.fn.expand("%:e")
-    local icon = require("nvim-web-devicons").get_icon(file_name, file_ext, { default = true })
+  local file_name, file_ext = vim.fn.expand("%:t"), vim.fn.expand("%:e")
+  local icon = require("nvim-web-devicons").get_icon(file_name, file_ext, { default = true })
 
-    local fileName = vim.fn.pathshorten(vim.fn.fnamemodify(name, ":."))
-    fileName = icon .. " " .. fileName
+  local fileName = vim.fn.pathshorten(vim.fn.fnamemodify(name, ":."))
+  fileName = icon .. " " .. fileName
 
-    return fileName
+  return fileName
 end
 
 local function getGitIcon()
-    return require("nvim-web-devicons").get_icon ".gitattributes"
+  return require("nvim-web-devicons").get_icon ".gitattributes"
 end
 
 function statusInfo.getGitInfo()
-    local git_branch = vim.fn["FugitiveHead"]()
+  local git_branch = vim.fn["FugitiveHead"]()
 
-    if not git_branch or git_branch == "" then
-        git_branch = "[No Git]"
-    else
-        git_branch = " %s " .. git_branch
-        git_branch = string.format(git_branch, getGitIcon())
-    end
+  if not git_branch or git_branch == "" then
+    git_branch = "[No Git]"
+  else
+    git_branch = " %s " .. git_branch
+    git_branch = string.format(git_branch, getGitIcon())
+  end
 
-    return git_branch
+  return git_branch
 end
 
 function statusInfo.getLineInfo()
-    local line = vim.fn.line(".")
-    local offset = vim.fn.col(".")
+  local line = vim.fn.line(".")
+  local offset = vim.fn.col(".")
 
-    return string.format("[%d:%s]", line, offset)
+  return string.format("[%d:%s]", line, offset)
 end
 
 function statusInfo.getMode()
-    -- Every so often this sends an error need to think of a way to fix this
-    local mode = vim.fn.mode()
+  -- Every so often this sends an error need to think of a way to fix this
+  local mode = vim.fn.mode()
 
-    local mode_table = {
-        ["n"] = "Normal",
-        ["i"] = "Insert",
-        ["v"] = "Visual",
-        ["c"] = "Command",
-        ["cv"] = "Command",
-        ["V"] = "Visual",
-        [""] = "Visual",
-    }
+  local mode_table = {
+    ["n"] = "Normal ",
+    ["i"] = "Insert ",
+    ["v"] = "Visual ",
+    ["c"] = "Command ",
+    ["cv"] = "Command ",
+    ["V"] = "Visual ",
+    [""] = "Visual ",
+    ["t"] = "Terminal ",
+  }
 
-    if mode_table[mode] then
-        mode = mode_table[mode]
-    end
+  if mode_table[mode] then
+    mode = mode_table[mode]
+  end
 
-    if mode == "Insert" then
-        vim.api.nvim_set_hl(0, "Modes", { fg = "#F2F2F2", bg = "#7fa3c0" })
-    else
-        vim.api.nvim_set_hl(0, "Modes", { fg = "#373b40", bg = "#7fa3c0" })
-    end
+  if mode ~= "Insert " then
+    vim.api.nvim_set_hl(0, "Modes", { fg = "#7fa3c0", bg = "#2e2e2e" })
+  else
+    vim.api.nvim_set_hl(0, "Modes", { fg = "#2e2e2e", bg = "#7fa3c0" })
+  end
 
-    return mode
+  return mode
 end
 
 function statusInfo.getFileType()
-    local file_name, file_ext = vim.fn.expand("%:t"), vim.fn.expand("%:e")
-    local icon = require 'nvim-web-devicons'.get_icon(file_name, file_ext, { default = true })
-    local filetype = vim.bo.filetype
+  local file_name, file_ext = vim.fn.expand("%:t"), vim.fn.expand("%:e")
+  local icon = require 'nvim-web-devicons'.get_icon(file_name, file_ext, { default = true })
+  local filetype = vim.bo.filetype
 
-    if filetype == '' then return "(no filetype)" end
-    return string.format(" %s %s ", icon, filetype):lower()
+  if filetype == '' then return "(no filetype)" end
+  return string.format(" %s %s ", icon, filetype):lower()
 end
 
 return statusInfo
